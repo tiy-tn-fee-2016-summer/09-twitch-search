4a8eacfd116916c00aea6e291325d0b55410410f
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-30 12:48:37 -0500

    Tests for create and add message item

---
+++ tests/exercise/01-create-message-item-test.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  function fixWhitespace(str) {
+    return str.replace(/\s+/g, ' ').trim();
+  }
+
+  /* global require */
+  const createMessageItem = require('create-message-item');
+
+  QUnit.module('Create Message Item');
+
+  test('it creates a new message item element', (assert) => {
+    const messageOne = { username: 'Ryan', message: 'Do your homework' };
+    const messageTwo = { username: 'Josh', message: 'Time for standups' };
+
+    const resultOne = createMessageItem(messageOne);
+    const resultTwo = createMessageItem(messageTwo);
+
+    // Element tag names are uppercase when looked up
+    assert.equal(resultOne.tagName, 'LI', 'createMessageItem should return an `LI` element');
+
+    // Uses the fixWhitespace helper to remove repeating whitespace
+    // so you can format your HTML better
+    assert.equal(fixWhitespace(resultOne.innerText), 'Ryan Do your homework',
+      'createMessageItem should fill in the required data for messageOne');
+    assert.equal(fixWhitespace(resultTwo.innerText), 'Josh Time for standups',
+      'createMessageItem should fill in the required data for messageTwo');
+
+    const nameOne = fixWhitespace(resultOne.querySelector('.message-item__username').innerText);
+    const nameTwo = fixWhitespace(resultTwo.querySelector('.message-item__username').innerText);
+
+    assert.equal(nameOne, 'Ryan',
+      'createMessageItem should create a ".message-item__username" span filled with the username for messageOne');
+    assert.equal(nameOne, 'Josh',
+      'createMessageItem should create a ".message-item__username" span filled with the username for messageTwo');
+  });
+})();
---
+++ tests/exercise/02-add-message-item-test.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  function fixWhitespace(str) {
+    return str.replace(/\s+/g, ' ').trim();
+  }
+
+  /* global require */
+  const addMessageItem = require('add-message-item');
+
+  QUnit.module('Add Message Item');
+
+  test('it adds a new message item element to a list element', (assert) => {
+    const list = document.createElement('ul');
+    const messageOne = { username: 'Ryan', message: 'Do your homework' };
+    const messageTwo = { username: 'Josh', message: 'Time for standups' };
+
+    // Typeof checks that something is a function, number, etc
+    assert.equal(typeof addMessageItem, 'function',
+      'The add message item module should export a function');
+    // function lengths returns a number of arguments the function accepts
+    assert.equal(addMessageItem.length, 2,
+      'addMessageItem should take two arguments, a list element and an object of data');
+
+    addMessageItem(list, messageOne);
+    addMessageItem(list, messageTwo);
+
+    // Looks that the two messages have been added to the list
+    const resultOne = list.querySelector('li');
+    const resultTwo = list.querySelector('li:last-of-type');
+
+    // Element tag names are uppercase when looked up
+    assert.equal(resultOne.tagName, 'LI', 'createMessageItem should return an `LI` element');
+
+    // Uses the fixWhitespace helper to remove repeating whitespace
+    // so you can format your HTML better
+    assert.equal(fixWhitespace(resultOne.innerText), 'Ryan Do your homework',
+      'createMessageItem should fill in the required data for messageOne');
+    assert.equal(fixWhitespace(resultTwo.innerText), 'Josh Time for standups',
+      'createMessageItem should fill in the required data for messageTwo');
+  });
+})();
